f53f845ba8bc05a87723b825f1e63e3d7ce8d70b
hossamgamal-1 <hossamgmal1313@gmail.com>
2023-03-23 17:14:14 +0200

    migration from provider to bloc_2

--- lib/services/chat/presentation/widgets/chat_messages.dart
+++ lib/services/chat/presentation/widgets/chat_messages.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/dependency_injection.dart';
 import '../../business_logic/chat_cubit/chat_cubit.dart';
 import 'recieved_message_tile.dart';
 import 'sent_message_tile.dart';
@@ -12,38 +13,41 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance
-            .collection('chat/roH5hykQCCKmM2ilEVtp/messages')
-            .orderBy('time', descending: true)
-            .snapshots(),
-        builder: (context, snapShots) {
-          if (snapShots.connectionState == ConnectionState.waiting) {
-            return const Center(
-                child: CircularProgressIndicator(color: Colors.white));
-          }
+    return BlocProvider(
+      create: (context) => LogicAuthCubit(sL()),
+      child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: FirebaseFirestore.instance
+              .collection('chat/roH5hykQCCKmM2ilEVtp/messages')
+              .orderBy('time', descending: true)
+              .snapshots(),
+          builder: (context, snapShots) {
+            if (snapShots.connectionState == ConnectionState.waiting) {
+              return const Center(
+                  child: CircularProgressIndicator(color: Colors.white));
+            }
 
-          final List<QueryDocumentSnapshot<Map<String, dynamic>>> docs =
-              snapShots.data!.docs;
-          return SingleChildScrollView(
-              controller: context.watch<ChatCubit>().scrollController,
-              child: Column(
-                  children: docs
-                      .map<Widget>((snapShotItem) {
-                        return snapShotItem['senderId'] ==
-                                context.read<LogicAuthCubit>().currentUserUID
-                            ? Container(
-                                alignment: Alignment.centerRight,
-                                child: SentMessageTile(snapShotItem),
-                              )
-                            : Align(
-                                alignment: Alignment.centerLeft,
-                                child: RecievedMessageTile(snapShotItem),
-                              );
-                      })
-                      .toList()
-                      .reversed
-                      .toList()));
-        });
+            final List<QueryDocumentSnapshot<Map<String, dynamic>>> docs =
+                snapShots.data!.docs;
+            return SingleChildScrollView(
+                controller: context.watch<ChatCubit>().scrollController,
+                child: Column(
+                    children: docs
+                        .map<Widget>((snapShotItem) {
+                          return snapShotItem['senderId'] ==
+                                  context.read<LogicAuthCubit>().currentUserUID
+                              ? Container(
+                                  alignment: Alignment.centerRight,
+                                  child: SentMessageTile(snapShotItem),
+                                )
+                              : Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: RecievedMessageTile(snapShotItem),
+                                );
+                        })
+                        .toList()
+                        .reversed
+                        .toList()));
+          }),
+    );
   }
 }
